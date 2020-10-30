a58746d0d776bff57867c268283d90d9a194833b
hayungdewanto <hayunngdewamto9@gmail.com>
2020-10-30 16:01:51 +0700

    Tufas 6

---
+++ e41190934_hayungdewanto/Tugas 6/type_data.php
@@ -0,0 +1,26 @@
+<?php
+//type data char (karakter)
+$jenis_kelamin ='L';
+
+//type data string (teks)
+$nama_lengkap ="Petani Kode ";
+
+//type data integer
+$umur = 20;
+
+//type data float
+$berat = 48.5;
+
+//type data float
+$tinggi= 48.5;
+
+//type data boolean
+$menikah = false;
+
+echo "Nama = $nama_lengkap <br>";
+echo "Jenis Kelamin = $jenis_kelamin <br>";
+echo "Umur = $umur tahun<br>";
+echo "Berat badan = $berat kg<br>";
+echo "Tinggi badan = $tinggi cm<br>";
+echo "Menikah = $menikah"; 
+?>
