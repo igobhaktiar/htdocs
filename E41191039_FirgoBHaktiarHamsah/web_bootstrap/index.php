f05e6dc9ea1de2776d0c6af0d61237b5c2f6da7b
igobhaktiar <71805833+igobhaktiar@users.noreply.github.com>
2020-12-03 15:25:45 +0700

    login

--- E41191039_FirgoBHaktiarHamsah/web_bootstrap/index.php
+++ E41191039_FirgoBHaktiarHamsah/web_bootstrap/index.php
@@ -214,10 +214,8 @@
 
         </nav>
         <!-- End of Topbar -->
-
         <!-- Begin Page Content -->
         <div class="container-fluid">
-
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
@@ -225,6 +223,7 @@
           </div>
 
           <!-- Content Row -->
+
           <div class="row">
 
             <!-- Earnings (Monthly) Card Example -->
