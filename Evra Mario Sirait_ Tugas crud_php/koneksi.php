d25145bdf2f7ee73f34cf369a48a1b32de701f55
Dwi Wahyu Prambodo <71806064+DwiWahyuP@users.noreply.github.com>
2020-12-10 08:04:56 +0700

    evra

--- Evra Mario Sirait_ Tugas crud_php/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$koneksi = mysqli_connect ("localhost","root","","crud_web");
-
-    if (mysqli_connect_error()){
-    echo "Koneksi Database Gagal : " . mysqli_connect_error ();
-    }
-
-?>
