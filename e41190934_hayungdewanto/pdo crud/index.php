96c25fa5ad4018ce47b2be137fa23695ade44808
hayungdewanto <hayunngdewamto9@gmail.com>
2020-12-29 18:07:54 +0700

    pdo crud

---
+++ e41190934_hayungdewanto/pdo crud/index.php
@@ -0,0 +1,11 @@
+<?php include "templates/header.php";?>
+					<ul>
+						<li>
+							<a href="create.php"><strong>Buat User</strong></a> - tambahkan pengguna
+						</li>
+						
+						<li>
+							<a href="read.php"><strong>Cari User</strong></a> - cari pengguna
+						</li>
+					</ul>
+					<?php include "templates/footer.php"; ?>
---
+++ e41190934_hayungdewanto/pdo crud/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta http-equiv="x-ua-compatible" content="ie=edge" />
+		<meta nam="viewport" content="width=device-width, initial-scale=1"/>
+		
+			<title>Database Sederhana App</title>
+			
+				<link rel="stylesheet" href="css/bootstrap2.css" />
+				</head>
+				
+				<body>
+					<h1>PDO</h1>
+				</body>
+				</html>
